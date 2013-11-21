1489ef668b6b394f6918a964b7a5ef72f22880dd
Robear Geleta <rgeleta@colgate.edu>
2013-11-20 20:56:10 -0500

    adding socket works

--- mylist.c
+++ mylist.c
@@ -8,14 +8,13 @@
 
 struct node {
   int socket;
-  struct node * prev ;
+  struct node * next;
 
 
 };
 
 typedef struct{
   struct node *head ;
-
   pthread_mutex_t lock ;
   pthread_cond_t signal  ; 
 }list_t ;
@@ -30,26 +29,36 @@ void list_init(list_t *list){
 
 }
 
-void add_head ( list_t * list){
+void add_head ( list_t * list, int sock){
   pthread_mutex_lock(&(list-> lock));
+  
   struct node * new_node = (struct node * ) malloc(sizeof(struct node));
+  new_node -> socket = sock; 
   if (! new_node){
     printf("\n Didn' malloc\n");
     abort();
 }
+  
   struct node * tmp =  list -> head  ; 
-  new_node = tmp ; 
-  list-> head = new_node ; 
-
+  if ( tmp !=NULL ){
+  new_node -> next  = tmp ; 
+  list-> head  = new_node ; 
+  new_node -> next = tmp ; 
+  }else{
+    new_node ->next = NULL;
+    list-> head = new_node ;
+  }
    pthread_mutex_unlock(&(list-> lock));
 }
 
 void print_ll( list_t * list){
    pthread_mutex_lock(&(list-> lock));
+
    struct node * itr = list -> head ;
+
    while (itr != NULL ){
      printf("\n socket: %d\n",itr -> socket);
-     itr ++;
+     itr = itr -> next ;
      
    }
    pthread_mutex_unlock(&(list-> lock));
@@ -57,6 +66,10 @@ void print_ll( list_t * list){
 
 
 int main (){
-  
+  list_t job_list ;
+  list_init(& job_list);
+  add_head( &job_list, 1);
+  add_head(&job_list, 2);
+  print_ll(&job_list);
   return 0 ;
 }
